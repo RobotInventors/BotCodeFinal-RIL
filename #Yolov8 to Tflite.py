19ef2ef6ca2b8dbaa0eaa85600d63d588cce284e
RobotInventors <robotinventors03@gmail.com>
2024-12-19 14:45:06 +0530

    Add files via upload

---
+++ #Yolov8 to Tflite.py
@@ -0,0 +1,14 @@
+#Yolov8 to Tflite
+from ultralytics import YOLO
+
+# Load the YOLO11 model
+model = YOLO("best.pt")
+
+# Export the model to TFLite format
+model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
+
+# Load the exported TFLite model
+tflite_model = YOLO("yolo11n_float32.tflite")
+
+# Run inference
+results = tflite_model("https://ultralytics.com/images/bus.jpg")
